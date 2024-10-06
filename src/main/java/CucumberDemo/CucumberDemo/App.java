db5416023489672634d7b3f8173e8b699cdc575d
GitDemo <10am.seleniumjavabatch@gmail.com>
2024-10-06 21:15:22 +0530

    demo commit

---
+++ src/main/java/CucumberDemo/CucumberDemo/App.java
@@ -0,0 +1,10 @@
+package CucumberDemo.CucumberDemo;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
